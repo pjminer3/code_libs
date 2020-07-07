33a30abd540160fe43493cb147aa617d4e8281c4
Patrick Miner <patrickminer318@gmail.com>
2020-07-06 21:46:09 -0700

    Hooked server up to serve client/index.html

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+const path = require("path");
 const app = require("express")();
 const http = require("http").createServer(app);
 
 app.get("/", (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(path.join(__dirname + "/client/index.html"));
 });
 
 http.listen(3000, () => {
